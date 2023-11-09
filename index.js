324859fd7eb79d45e30e9486915d38f3ba0df959
aishbelle <aashibansal02@gmail.com>
2023-11-09 11:50:35 +0530

    make it better

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/relationships", relationshipRoutes);
 
-app.listen(8800, () => {
+app.listen(process.env.PORT || 8800, () => {
   console.log("API working!");
 });
